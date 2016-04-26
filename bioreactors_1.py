ead8a14ef3d54f510538f2062e2da27688f86de1
Fana Teffera <fteffera@gmail.com>
2016-04-25 22:09:56 -0500

    Create bioreactors_1.py

---
+++ bioreactors_1.py
@@ -0,0 +1,10 @@
+class bioreactors_1:
+  # Assumption of enviroment: python 3.5.1
+  # Please look into detailed comment for each modules to see the function as well as the assumption it was based on. 
+  # All functions will be called in another class <will be added later>
+  
+  def reactors_choices:
+  
+    pass
+
+
